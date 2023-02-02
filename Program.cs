6c48fc0f377fe85d21d1276daa780b150acd3f03
ASPNyan <sebastian.ricketts2210@gmail.com>
2023-02-03 08:12:49 +1030

    Add separate Name.css for ease of reading

--- Program.cs
+++ Program.cs
@@ -2,10 +2,10 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SebastianRicketts.dev;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+var Builder = WebAssemblyHostBuilder.CreateDefault(args);
+Builder.RootComponents.Add<App>("#app");
+Builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+Builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(Builder.HostEnvironment.BaseAddress) });
 
-await builder.Build().RunAsync();
+await Builder.Build().RunAsync();
