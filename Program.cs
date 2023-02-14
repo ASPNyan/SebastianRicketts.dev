00f437e835b708a6777363becaaef5e6c3cfc03f
ASPNyan <sebastian.ricketts2210@gmail.com>
2023-02-14 15:47:20 +1030

    Well I totally didn't forget to push my commits so I can work on them elsewhere :/

--- Program.cs
+++ Program.cs
@@ -7,5 +7,6 @@ Builder.RootComponents.Add<App>("#app");
 Builder.RootComponents.Add<HeadOutlet>("head::after");
 
 Builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(Builder.HostEnvironment.BaseAddress) });
+Builder.Services.AddSingleton<State>();
 
 await Builder.Build().RunAsync();
---
+++ State.cs
@@ -0,0 +1,23 @@
+namespace SebastianRicketts.dev;
+
+public class State
+{
+    public Component Current = Component.Startup;
+    
+    public enum Component
+    {
+        Startup,
+        Transition,
+        Name
+    }
+
+    public event Action? OnChange;
+
+    public void Set(Component Component)
+    {
+        Current = Component;
+        StateChanged();
+    }
+
+    private void StateChanged() => OnChange?.Invoke();
+}
